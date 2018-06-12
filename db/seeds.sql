547e86e07d241ef6860d116615aac81294eac627
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-06-12 14:36:39 -0700

    add data to static bucket table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,24 @@
-INSERT INTO buckets (name, photo) VALUES ("Recipe", "https://via.placeholder.com/250x150"),
-("Fashion" "https://via.placeholder.com/350x150.jpg");
-INSERT INTO bookmarks (name, description, priority, photos) VALUES ("Niyati's", "A hamburger, beefburger or burger is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, barbecued, or flame broiled. ... A hamburger topped with cheese is called a cheeseburger.", 4, "https://via.placeholder.com/350x150");
-INSERT INTO users (name) VALUES ("Yumna");
+-- INSERT INTO buckets (name, photo) VALUES ("Recipe", "https://via.placeholder.com/250x150"),
+-- ("Fashion" "https://via.placeholder.com/350x150.jpg");
+-- INSERT INTO bookmarks (name, description, priority, photos) VALUES ("Niyati's", "A hamburger, beefburger or burger is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, barbecued, or flame broiled. ... A hamburger topped with cheese is called a cheeseburger.", 4, "https://via.placeholder.com/350x150");
+-- INSERT INTO users (name) VALUES ("Yumna");
+
+
+USE bucketodo_DB;
+
+INSERT INTO bucketodo_DB.Buckets (name, photo, createdAt, updatedAt) 
+VALUES  ("Recipes", "https://unsplash.com/search/photos/recipes", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Fashion", "https://unsplash.com/search/photos/fashion", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Craft", "https://unsplash.com/search/photos/craft", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Travel", "https://unsplash.com/search/photos/travel", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Party", "https://unsplash.com/search/photos/party", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Wedding", "https://unsplash.com/search/photos/wedding", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Humor", "https://unsplash.com/search/photos/funny", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Educational", "https://unsplash.com/search/photos/educational", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Music", "https://unsplash.com/search/photos/music", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Health", "https://unsplash.com/search/photos/health", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Life Hacks", "https://unsplash.com/search/photos/hacks", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Sports", "https://unsplash.com/search/photos/sports", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Animals", "https://unsplash.com/search/photos/animals", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Cars", "https://unsplash.com/search/photos/cars", "2018-01-01 01:00:00", "2018-01-01 01:00:00"),
+        ("Electronics", "https://unsplash.com/search/photos/electronics", "2018-01-01 01:00:00", "2018-01-01 01:00:00");
