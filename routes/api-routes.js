b5680cdbb1321713ab576a2f0a7376d2a3024b62
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-06-14 09:41:22 -0700

    clean code, add delete and update skeleton

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,6 +6,7 @@ var db = require("../models");
 // =============================================================
 module.exports = function (app) {
 
+    //CREATE new user
     app.post("/api/user", function (req, res) {
         console.log("/api/user");
         console.log(req.body);
@@ -17,25 +18,47 @@ module.exports = function (app) {
 
     });
 
+    //CREATE bucket for a user
+    app.post("/api/user/:id/bucket", function (req, res) {
+        console.log("meow meow helo")
+        console.log(req.body);
+        db.Buckets.create(req.body)
+            .then(function (dbBucket) {
+                console.log("dbBucket: ", dbBucket);
+                res.json(dbBucket);
+            });
+    });
+
+    //CREATE new bookmark for a bucket belonging to user
+    app.post("/api/user/:id/", function (req, res) {
+        db.Bookmarks.create(req.body)
+            .then(function (dbBookmarks) {
+                res.json(dbBucket);
+            });
+        // res.redirect("/")
+        // console.log("/user/:id/createBookmark");
+    });
+
+    //CHECK if login info exists
     app.post("/signin", function (req, res) {
         console.log("hi there!!");
-        db.Users.findOne({ 
+        db.Users.findOne({
             where: {
-                email: req.body.email, 
-                password:req.body.password
-            } 
-        }).then(function(result) {
-        
+                email: req.body.email,
+                password: req.body.password
+            }
+        }).then(function (result) {
+
             if (!result) {
                 res.json(false);
             } else {
                 res.json(result);
             }
-         });
-    
+        });
+
     });
 
-    //show all users in database
+    //GET all users in database
     app.get("/api/users", function (req, res) {
         db.Users.findAll({})
             .then(function (dbUsers) {
@@ -43,9 +66,8 @@ module.exports = function (app) {
             })
     })
 
+    //GET all buckets belonging to a user
     app.get("/api/user/:id/bucket", function (req, res) {
-        // show all the buckets belonging to a user
-        //the work of query SQL with sequalize
         console.log("meow meow")
         db.Buckets.findAll({
             where: {
@@ -58,25 +80,8 @@ module.exports = function (app) {
         console.log("/user/:id/bucket");
     });
 
-    ///
-    app.get("/api/user/:id/buckets", function (req, res) {
-        // show all the buckets belonging to a user
-        //the work of query SQL with sequalize
-        console.log("meow meow buckets")
-        db.Buckets.findAll({
-            where: {
-                Userid: req.params.id
-            }
-        })
-            .then(function (dbBuckets) {
-                res.json(dbBuckets)
-            })
-    });
-    ///
-
+    //GET all bookmarks belonging to buckets of a user
     app.get("/api/user/:id/bucket/:bucketId/bookmark", function (req, res) {
-        // all bookmarks for that bucket for a specific user
-        //the work of query SQL with sequalize
         db.Bookmarks.findAll({
             where: {
                 UserId: req.params.id,
@@ -89,29 +94,52 @@ module.exports = function (app) {
         console.log("/user/:id/:bucketID/bookmark");
     });
 
-    app.post("/api/user/:id/bucket", function (req, res) {
-        // get user id form req.params 
-        console.log("meow meow helo")
-        console.log(req.body);
-        db.Buckets.create(req.body)
-            .then(function (dbBucket) {
-                console.log("dbBucket: ", dbBucket);
-                res.json(dbBucket);
-            });
-        // creating a bucket for a specific user
-        //the work of query SQL with sequalize
+    //UPDATE bucket for a specific user
+    app.put("/api/user/:id/bucket", function (req, res) {  //is the route correct?
+        db.Buckets.update(req.body, {
+            where: {
+                //how to catch the exact bucket to edit
+            }
+        })
+        .then(function (dbBuckets) {
+            res.json(dbBuckets)
+        });
     });
 
-    app.post("/api/user/:id/", function (req, res) {
-        // get bucket id form req.body
-        // creating a new bookmoark for a user in a specific bucket
-        //the work of query SQL with sequalize
-
-        res.redirect("/")
-        console.log("/user/:id/createBookmark");
+    //UPDATE a bookmark in a bucket, for specific user
+    app.put("/user/:userId/bucket/:bucketId/bookmark/:bookmarkId", function (req, res) {
+        db.Bookmarks.update(req.body, {
+            where: {
+                //how to catch the exact bucket to edit
+            }
+        })
+        .then(function (dbBookmarks) {
+            res.json(dbBookmarks)
+        });
     });
 
+    //DELETE bucket for a specific user
+    app.delete("/user/:userId/bucket/:bucketId", function (req, res) {
+        db.Buckets.destroy({
+            where: {
+                //how to catch the exact bucket to delete
+            }
+        })
+        .then(function (dbBuckets) {
+            res.json(dbBuckets)
+        });
+    });
 
-
-
+    //DELETE a bookmark in a bucket, for specific user
+    app.delete("/user/:userId/bucket/:bucketId/bookmark/:bookmarkId", function (req, res) {
+        db.Bookmarks.destroy({
+            where: {
+                //how to catch the exact bucket to delete
+            }
+        })
+        .then(function (dbBookmarks) {
+            res.json(dbBookmarks)
+        });
+    });
 }
+
