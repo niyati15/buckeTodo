fe9bb947b169981d10de080cfa997f0676e28b01
lesleyfon <lesleybanadzem2017@gmail.com>
2018-06-13 18:17:39 -0700

    added a card header

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,22 @@
-$(function () {
+$(function() {
 	//on click of login tag
-	$('#login-form-link').click(function (e) {
+    $('#login-form-link').click(function(e) {
 		$("#login-form").delay(100).fadeIn(100);
-		$("#register-form").fadeOut(100);
+ 		$("#register-form").fadeOut(100);
 		$('#register-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
 	//on click on register tab
-	$('#register-form-link').click(function (e) {
+	$('#register-form-link').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
-		$("#login-form").fadeOut(100);
+ 		$("#login-form").fadeOut(100);
 		$('#login-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
 	//on click of log in button
-	$(".btn-login").click(function (e) {
+	$(".btn-login").click(function(e) {
 		// e.preventDefault();
 		var username = $(".username-submit").val().trim();
 		var password = $(".password-submit").val().trim();
@@ -25,14 +25,14 @@ $(function () {
 
 	});
 	//on click of register button
-	$(".btn-register").click(function (e) {
+	$(".btn-register").click(function(e) {
 		e.preventDefault();
 		var username = $(".username-register").val().trim();
 		var email = $(".email-register").val().trim();
 		var password1 = $(".password-register").val().trim();
 		var password2 = $(".confirm-register").val().trim();
-
-		if (password1 === password2) {
+		
+		if (password1 === password2){
 			// alert(username + email + password1 + password2);
 			$(".btn-register").attr("href", "#")
 		}
@@ -42,10 +42,7 @@ $(function () {
 
 	});
 
-
-	var userID = sessionStorage.getItem('id');
-	console.log(userID);
-	$.get("api/user/" + userID + "/bucket", function (res) {
+	$.get("api/user/1/bucket", function (res) {
 		iteration(res);
 	});
 
@@ -74,15 +71,15 @@ $(function () {
 	function appendHTML(bucketName) {
 		// console.log(bucketName);
 		var incoming = $(".container");
-		var bucketView = "<div class='card addBucket'>" +
-
-							"<div class='card-body crtBckt' href='#'>" + bucketName + "</div>" +
+		var bucketView = "<div class='card addBucket' ><a href='#'>" +
+						 "<div class='card-header'><strong>" + bucketName + "</strong></div>" +
+							"<div class='card-body crtBckt' href='#'>"  + "</div>" +
 					
 						"<div class='card-footer'>" +
-						"<a href='#' class='card-link'><strong>" + "View Bucket" + "</strong></a>" + "<br>" +
-						"<a href='#' class='card-link'><strong>" + "Add to Bucket"+ "</strong></a>" +
+						"<a href='#' class='card-link'><strong>" + "View " + bucketName+ "</strong></a>" + "<br>" +
+						"<a href='#' class='card-link'><strong>" + "Add to "+ bucketName +"</strong></a>" +
 						"</div>" +
-						"</div>" 
+						"</a></div>" 
 		incoming.append(bucketView);
 	}
 
